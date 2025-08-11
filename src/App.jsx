ac30de59702ffa59a073b9da45cf52cde1638aaa
MatteoRovera <matteorovera06@gmail.com>
2025-08-11 15:03:59 -0400

    Updated starting language

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function SongInputForm({ onSearchComplete }) {
               Language
             </label>
             <div className="px-3 py-2 bg-gray-100 border border-gray-300 rounded-md text-gray-700">
-              🇫🇷 French
+              🇺🇸 English
             </div>
           </div>
 
